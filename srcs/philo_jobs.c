f100a77544dd86bfcf94522d2e16c2baf50a4b0f
Gabriel De almeida torres <gade-alm@c1r10s2.42lisboa.com>
2023-01-06 14:08:35 +0000

    Philo sleeping

--- srcs/philo_jobs.c
+++ srcs/philo_jobs.c
@@ -6,19 +6,25 @@
 /*   By: gade-alm <gade-alm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 17:32:45 by gade-alm          #+#    #+#             */
-/*   Updated: 2023/01/06 12:55:19 by gade-alm         ###   ########.fr       */
+/*   Updated: 2023/01/06 14:02:44 by gade-alm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
 
-// void	philo_nap(t_philo *philos)
-// {
-// 	long int	nap_time;
+void	philo_nap(t_philo *philos)
+{
+	long int	nap_time;
+	long int	napping;
 
-// 	nap_time = get_time();
-// 	if ()
-// }
+	nap_time = get_time();
+	napping = 0;
+	print_message(philos, "is sleeping");
+	usleep(100);
+	while (napping <= data_call()->sleep_time && philo_alive(philos))
+		napping = get_time() - nap_time;
+	return ;
+}
 
 void	*philo_jobs(void *arg)
 {
@@ -30,7 +36,7 @@ void	*philo_jobs(void *arg)
 	philos->last_meal = data_call()->start;
 	while (philo_alive(philos))
 	{
-		// philo_nap(philos);
+		philo_nap(philos);
 		usleep(150);
 		if (philo_alive(philos))
 			print_message(philos, "isn't working");
